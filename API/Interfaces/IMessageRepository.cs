8e5e7d3c2f6376b63a40ad7224ba885ed8ee5a52
Silviu <Silviu@DESKTOP-MOAPUM6>
2020-11-22 00:44:10 +0200

    Add entity and repository for message

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+    }
+}
