048fe2b6720462cb97fdca87699e9225c63fd4b5
Silviu <Silviu@DESKTOP-MOAPUM6>
2020-11-21 12:52:53 +0200

    Male the Last Active filter more optimal

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using API.Entities;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -10,7 +12,16 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+
+        }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+
+            var balaur = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return  int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
         }
     }
 }
