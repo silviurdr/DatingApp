35b57485321b67ad01a54373a89a8028496b9fe4
Silviu <Silviu@DESKTOP-MOAPUM6>
2020-11-22 08:52:13 +0200

    Set the Angular app for messaging

---
+++ client/src/app/_services/message.service.ts
@@ -0,0 +1,20 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { getPaginatedResults, getPaginationHeaders } from './paginationHelper';
+import { Message } from '../_models/message';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMessages(pageNumber, pageSize, container) {
+    let params = getPaginationHeaders(pageNumber, pageSize);
+    params = params.append('Container', container);
+    return getPaginatedResults<Message[]>(this.baseUrl + 'messages', params, this.http);
+  }
+}
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,3 +1,6 @@
+import { MessageService } from './../_services/message.service';
+import { Pagination } from './../_models/pagination';
+import { Message } from './../_models/message';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(response => {
+      this.messages = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  pagedChanged(event: any) {
+    this.pagination = event.page;
+    this.loadMessages();
   }
 
 }
