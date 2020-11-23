b26545cb150ad3ee7029b2a5bc3d2c8c56bcfcf5
Silviu <Silviu@DESKTOP-MOAPUM6>
2020-11-23 15:19:05 +0200

    Add admin component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
@@ -25,6 +26,8 @@ const routes: Routes = [
       {path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
+      {path: 'admin', component: AdminPanelComponent},
+
     ]
   },
   {path: 'errors', component: TestErrorsComponent},
