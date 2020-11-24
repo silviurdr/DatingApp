ebe1d732089b3ff1f4070f74d7eb10d023ac2c91
Silviu <silviu.catrina@gmail.com>
2020-11-24 09:43:59 +0200

    Add a presence hub class

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -40,7 +40,6 @@ namespace API.Controllers
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
             userParams.CurrentUsername = user.UserName;
-
             if(string.IsNullOrEmpty(userParams.Gender)) {
                 userParams.Gender = user.Gender == "male"? "female": "male";
             }
