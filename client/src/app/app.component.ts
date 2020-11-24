245bdb957e0daf88ea54016534f389bac88ec05e
Silviu <silviu.catrina@gmail.com>
2020-11-24 10:35:36 +0200

    Add a presence tracker

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { PresenceService } from './_services/presence.service';
 import { User } from './_models/user';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
@@ -12,7 +13,7 @@ export class AppComponent {
   title = 'The Dating app';
   users: any;
 
-  constructor( public accountService: AccountService) {}
+  constructor( public accountService: AccountService, private presence: PresenceService) {}
 
   ngOnInit() {
     this.setCurrentUser();
@@ -21,7 +22,11 @@ export class AppComponent {
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
-    this.accountService.setCurrentUser(user);
+    if (user) {
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
+
   }
 
 }
